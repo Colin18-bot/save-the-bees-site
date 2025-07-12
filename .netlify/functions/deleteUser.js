d2f99ac4338ea33d8188e2d56979a757d3e38cb1
Colin Chorley <chorleyc@hotmail.co.uk>
2025-07-13 00:19:45 +0100

    Update deleteUser.js

--- .netlify/functions/deleteUser.js
+++ .netlify/functions/deleteUser.js
@@ -5,6 +5,10 @@ import { createClient } from '@supabase/supabase-js'
 const supabaseUrl = 'https://uihngfpmoasnofyrvpmw.supabase.co'
 const supabaseServiceRoleKey = process.env.SUPABASE_SERVICE_ROLE_KEY
 
+if (!supabaseServiceRoleKey) {
+  throw new Error('SUPABASE_SERVICE_ROLE_KEY is missing from environment variables')
+}
+
 const supabase = createClient(supabaseUrl, supabaseServiceRoleKey)
 
 export async function handler(event) {
