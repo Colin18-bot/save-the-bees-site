982fc02a88dda2d776b2a9f963944880eb0f134a
Colin Chorley <chorleyc@hotmail.co.uk>
2025-07-27 14:50:31 +0100

    update

--- member-area/js/login-register.js
+++ member-area/js/login-register.js
@@ -2,9 +2,9 @@ document.addEventListener("DOMContentLoaded", () => {
   // ==== Supabase Config ====
   const SUPABASE_URL = 'https://uihngfpmoasnofyrvpmw.supabase.co';
   const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVpaG5nZnBtb2Fzbm9meXJ2cG13Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTIzNzE2MTEsImV4cCI6MjA2Nzk0NzYxMX0.JO8y5G4lxGoyJozZfyxK-8VkJ5UusQzzkQxEYy8RVGo';
-  const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+  const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
-  // ==== Show/Hide Password Toggle (Login) ====
+   // ==== Show/Hide Password Toggle (Login) ====
   const passwordInput = document.getElementById("password");
   const togglePassword = document.getElementById("togglePassword");
 
