de7ffcc064d99d4c03aed38745ed41eeb66a1736
Colin Chorley <chorleyc@hotmail.co.uk>
2025-11-20 21:08:40 +0000

    Update Register.jsx

--- beekeeper-dev/src/pages/Register.jsx
+++ beekeeper-dev/src/pages/Register.jsx
@@ -120,7 +120,7 @@ const Register = () => {
             className="w-full border border-gray-300 px-3 py-2 rounded bg-blue-50 focus:outline-none focus:ring-2 focus:ring-green-500"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
-            placeholder="name@example.com"
+            placeholder="e.g. name@example.com"
             required
           />
         </div>
