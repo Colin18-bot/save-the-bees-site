3b24be2506251047873b5221d5a9caafc07e7238
Colin Chorley <chorleyc@hotmail.co.uk>
2025-11-21 00:18:57 +0000

    Update Contact.jsx

--- beekeeper-dev/src/pages/Contact.jsx
+++ beekeeper-dev/src/pages/Contact.jsx
@@ -64,7 +64,7 @@ export default function Contact() {
             type="email"
             required
             className="w-full border rounded px-3 py-2 bg-blue-50"
-            placeholder="you@example.com"
+            placeholder="e.g. name@example.com"
           />
         </div>
 
