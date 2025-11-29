a2aebf2c8e0dd66b54291686e8f192488dc90f05
Colin Chorley <chorleyc@hotmail.co.uk>
2025-11-29 19:43:23 +0000

    Update Sidebar.jsx

--- beekeeper-dev/src/components/Sidebar.jsx
+++ beekeeper-dev/src/components/Sidebar.jsx
@@ -4,7 +4,7 @@ import { NavLink, useNavigate } from "react-router-dom";
 import { supabase } from "../services/supabase";
 
 // APP_VERSION=1.2.3
-const APP_VERSION = "1.0.2";
+const APP_VERSION = "1.0.3";
 
 const Sidebar = ({ setIsMobileMenuOpen }) => {
   const [quickCreateOpen, setQuickCreateOpen] = useState(true);
