e6ae4a97c66060a0b9508a7daa6dc63edef1807b
Colin Chorley <chorleyc@hotmail.co.uk>
2026-01-01 23:54:47 +0000

    Update PrintReport.jsx

--- beekeeper-dev/src/pages/Reports/PrintReport.jsx
+++ beekeeper-dev/src/pages/Reports/PrintReport.jsx
@@ -735,7 +735,8 @@ export default function PrintReport() {
             </select>
           </div>
 
-          <div className="grid grid-cols-2 gap-2">
+         <div className="grid grid-cols-1 sm:grid-cols-2 gap-2">
+
             <div>
               <label className="block text-sm font-medium mb-1">From</label>
               <input
